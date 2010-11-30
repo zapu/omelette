05646080e30cc41cb56387f1400be7b0f1195a30
Michal Zochniak <michal@zapu.net>
2010-11-30 16:43:00 +0100

    Trying FontMetrics, see researchDrawable.py

--- omelette/fromage/modules/researchDrawable.py
+++ omelette/fromage/modules/researchDrawable.py
@@ -47,8 +47,12 @@ class DrawableRelation(QGraphicsLineItem):
         self.source = source
         self.target = target
         
+        self.__relationText = self.source.name + "-" + self.target.name
+        
         self.source.edges.append(self)
         self.target.edges.append(self)
+        
+        self.__textExtra = 0
         
         if(self.target.name == "barnex"):
             self.setPen(QPen(QColor(255, 0, 0), 1, Qt.DotLine, Qt.RoundCap, Qt.RoundJoin))
@@ -59,10 +63,34 @@ class DrawableRelation(QGraphicsLineItem):
         
     def update(self):
         self.setLine(QLineF(self.source.pos(), self.target.pos()))
-        pass
         
     def paint(self, painter, style, widget):
+        #matrix = QMatrix()
+        
+        srodekX = self.line().p1().x() + self.line().dx() / 2
+        srodekY = self.line().p1().y() + self.line().dy() / 2
+        
+        fontMetrics = painter.fontMetrics()
+        self.__textExtra = fontMetrics.width(self.__relationText)
+        
+        #matrix.translate(srodekX, srodekY)
+        #matrix.rotate(45)
+        #painter.setMatrix(matrix)
+        
+        #painter.drawText(QPointF(srodekX, srodekY), self.source.name + "-" + self.target.name)
+        painter.drawText(QPointF(srodekX - fontMetrics.width(self.__relationText) / 2, srodekY - 10), self.__relationText)
+        
+        #painter.setMatrix(QMatrix())
+        
         QGraphicsLineItem.paint(self, painter, style, widget)
-        pass
+        
+    def boundingRect(self):
+        #extra = (self.pen().width() + 20) / 2.0;
+        extra = self.__textExtra
+        
+        return QRectF(self.line().p1(), 
+                      QSizeF(self.line().p2().x() - self.line().p1().x(),
+                      self.line().p2().y() - self.line().p1().y())).normalized().adjusted(-extra, -extra, extra, extra);
+
     
     
